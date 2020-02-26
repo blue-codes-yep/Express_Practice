5f4a21357c605248c1da8d5afccccc3aa4e0c418
Blue Lindsey <linzblue5@gmail.com>
2020-02-26 10:39:43 -0500

    Added person controller

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rootController = require('./routes/index')
 const catsController = require('./routes/cat')
 const dogsController = require('./routes/dogs')
 const cats_and_dogs_controller = require('./routes/dogs_and_cats')
+const personController = require('./routes/person')
 
 app.listen(3333, () => {
     console.log('Server running on port 3333');
@@ -13,3 +14,4 @@ app.use('/', rootController);
 app.use('/cats', catsController);
 app.use('/dogs', dogsController);
 app.use('/cats_and_dogs', cats_and_dogs_controller);
+app.use('/person', personController);
