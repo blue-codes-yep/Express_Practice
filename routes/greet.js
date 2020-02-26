f1ae76b9e24289224ab8c31b4c50558fec50cec0
Blue Lindsey <linzblue5@gmail.com>
2020-02-26 11:19:29 -0500

    Made a greet route

---
+++ routes/greet.js
@@ -0,0 +1,12 @@
+const express = require('express'),
+    router = express.Router();
+
+router.get('/:name?', (req, res) => {
+    const {name} = req.params;
+    res
+        .status(200)
+        .send(`<h1> Hello, ${name}! </h1>`)
+        .end();
+});
+
+module.exports = router; 
