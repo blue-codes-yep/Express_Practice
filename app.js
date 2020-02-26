2d22c6870a8d9a560695ebad5780eb559dbbd34d
Blue Lindsey <linzblue5@gmail.com>
2020-02-26 11:19:09 -0500

    Added greet controller

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const catsController = require('./routes/cat')
 const dogsController = require('./routes/dogs')
 const cats_and_dogs_controller = require('./routes/dogs_and_cats')
 const personController = require('./routes/person')
+const greetController = require('./routes/greet')
 
 app.listen(3333, () => {
     console.log('Server running on port 3333');
@@ -15,3 +16,4 @@ app.use('/cats', catsController);
 app.use('/dogs', dogsController);
 app.use('/cats_and_dogs', cats_and_dogs_controller);
 app.use('/person', personController);
+app.use('/greet', greetController);
