e2e1b2297a65c04a6339575318f2edd9a035aee1
Blue Lindsey <linzblue5@gmail.com>
2020-02-26 14:24:46 -0500

    Added template engine, VIEW SIDE

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const express = require('express');
-const app = express();
-const rootController = require('./routes/index')
-const catsController = require('./routes/cat')
-const dogsController = require('./routes/dogs')
-const cats_and_dogs_controller = require('./routes/dogs_and_cats')
-const personController = require('./routes/person')
-const greetController = require('./routes/greet')
-const ageController = require('./routes/age')
+const express = require('express'),
+ app = express(),
+es6Renderer = require('express-es6-template-engine'),
+ rootController = require('./routes/index'),
+ catsController = require('./routes/cat'),
+ dogsController = require('./routes/dogs'),
+ cats_and_dogs_controller = require('./routes/dogs_and_cats'),
+ personController = require('./routes/person'),
+ greetController = require('./routes/greet'),
+ ageController = require('./routes/age');
+
+app.engine('html', es6Renderer);
+app.set('views','views');
+app.set('view engine','html');
+
 
 app.listen(3333, () => {
     console.log('Server running on port 3333');
