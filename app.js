08e66e441e81284a03181cae77aba24d15350875
Blue Lindsey <linzblue5@gmail.com>
2020-02-26 09:58:56 -0500

    Imported Controllers

--- app.js
+++ app.js
@@ -1,38 +1,15 @@
 const express = require('express');
 const app = express();
+const rootController = require('./routes/index')
+const catsController = require('./routes/cat')
+const dogsController = require('./routes/dogs')
+const cats_and_dogs_controller = require('./routes/dogs_and_cats')
 
 app.listen(3333, () => {
     console.log('Server running on port 3333');
 });
 
-app.get('/', (req, res) => {
-    const helloworld = `<h2> Hello World <h2>`;
-    res
-    .status(200)
-    .send(helloworld)
-    .end();
-})
-
-app.get('/cats', (req, res) => {
-    const catsText = `<h2> This is a cats page <h2>`;
-    res
-    .status(200)
-    .send(catsText)
-    .end();
-})
-
-app.get('/dogs', (req, res) => {
-    const dogsText = `<h2> This is a dogs page <h2>`;
-    res
-    .status(200)
-    .send(dogsText)
-    .end();
-})
-
-app.get('/cats_and_dogs', (req, res) => {
-    const dogs_and_cats_text = `<h2> This is a cats and dogs page <h2>`;
-    res
-    .status(200)
-    .send(dogs_and_cats_text)
-    .end();
-})
+app.use('/', rootController);
+app.use('/cats', catsController);
+app.use('/dogs', dogsController);
+app.use('/cats_and_dogs', cats_and_dogs_controller);
