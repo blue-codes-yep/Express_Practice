5d49c0b51a4d1ebe3fdad5aa9605f6ab81323afe
Blue Lindsey <linzblue5@gmail.com>
2020-02-27 11:41:16 -0500

    Added Ceo Model

---
+++ models/ceoModel.js
@@ -0,0 +1,19 @@
+const db = require('./conn');
+
+class ExecutiveList {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    static async getAll() {
+        try {
+            const response = await db.any(`SELECT * FROM ceos;`);
+            return response;
+        } catch (error) {
+            console.error('ERROR', error);
+        }
+    }
+}
+
+module.exports = ExecutiveList;
