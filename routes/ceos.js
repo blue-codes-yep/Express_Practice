97d74d3ac9b5fa81594419c12bf143a6675a1cc3
Blue Lindsey <linzblue5@gmail.com>
2020-02-27 13:54:55 -0500

    Refactoring

--- routes/ceos.js
+++ routes/ceos.js
@@ -2,9 +2,18 @@ const express = require('express'),
     router = express.Router()
     ceoModel = require('../models/ceoModel');
 
-router.get('/',  async (req, res) => {
-    const executiveData = await ceoModel.getAll();
-    console.log('executive data', executiveData);
+router.get('/:id?',  async (req, res) => {
+    const { id } = req.params;
+    let executiveData = [];
+
+    if (!!id) {
+        executiveData = await ceoModel.getById(id);
+    } else {
+        executiveData = await ceoModel.getAll();
+    };
+    
+    const singleExecutive = await ceoModel.getById(1);
+    console.log('single executive', singleExecutive);
 
     res.render('template', {
     locals: {
